8d551d1f1ffe267d6e58f61cc775b65a728161d1
David Sword <david.sword@automattic.com>
2020-06-17 16:09:04 -0700

    add manage jetpack

---
+++ manage-jetpack.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Plugin Name: davidsword.ca Jetpack Module Manager
+ * Plugin URI: https://github.com/davidsword/davidsword.ca-custom-plugins
+ * Description: davidsword.ca TwentyFifteen Theme Edits
+ * Version: 0.0.1
+ * Author: David Sword
+ * Author URI: https://davidsword.ca/
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+// Quickly see active modules in Admin (as there's no wpcli on Pressable).
+add_action(
+	'admin_notices',
+	function () {
+		if ( ! $_GET['jp_show_active_modules'] )
+			return;
+		?>
+		<div class="notice notice-info">
+			<p><?php echo 'Active JP Modules: ' . implode( ',', get_option( 'jetpack_active_modules' ) ); ?></p>
+		</div>
+		<?php
+	}
+);
+
+// Force modules off that aren't toggle'able in admin UI.
+add_filter( 'option_jetpack_active_modules', function ( $modules ) {
+	// @TODO make this a dynamic option.
+	$disabled_modules = array(
+		'stats',                 // using Google Analytics.
+		'woocommerce-analytics', // unused.
+		'tiled-gallery',         // unused.
+		'json-api',              // not nessisary.
+	);
+	foreach ( $disabled_modules as $module_slug ) {
+		$found = array_search( $module_slug, $modules );
+		if ( false !== $found )
+			unset( $modules[ $found ] );
+	}
+	return $modules;
+} );
