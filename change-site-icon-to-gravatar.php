a53f97072fb6027edd87499e8990ed8ee7063f84
David Sword <david.sword@automattic.com>
2020-10-26 16:51:45 -0700

    only use gravatar as a fallback if nothing else set

--- change-site-icon-to-gravatar.php
+++ change-site-icon-to-gravatar.php
@@ -11,30 +11,35 @@
  */
 
 /**
- * If no site icon - use the gravatar instead.
+ * If no site_icon - use the gravatar instead.
  */
 add_filter(
 	'get_site_icon_url',
-	function( $url ) {
-		return empty( $url ) ? dsca_get_gravatar_from_admin_email() : $url;
+	function( $url, $size ) {
+		return empty( $url ) ? dsca_get_gravatar_from_admin_email( $size ) : $url;
 	},
-	99,
-	1
+	999,
+	2
 );
 
+/**
+ * If no custom_logo - use the gravatar instead.
+ */
 add_filter(
 	'get_custom_logo',
-	function () {
-		$html = sprintf( '<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url"><img src="%2$s" class="custom-logo" /></a>',
-			esc_url( home_url( '/' ) ),
-			esc_url( dsca_get_gravatar_from_admin_email() )
-		);
+	function ( $html ) {
+		if ( empty( get_theme_mod( 'custom_logo' ) ) ) {
+			$html = sprintf( '<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url"><img src="%2$s" class="custom-logo" /></a>',
+				esc_url( home_url( '/' ) ),
+				esc_url( dsca_get_gravatar_from_admin_email() )
+			);
+		}
 		return $html;
 	},
 	999
 );
 
 
-function dsca_get_gravatar_from_admin_email() {
-	return 'https://www.gravatar.com/avatar/' . md5( get_option( 'admin_email' ) ) . '?s=512';
+function dsca_get_gravatar_from_admin_email( $size = 512 ) {
+	return 'https://www.gravatar.com/avatar/' . md5( get_option( 'admin_email' ) ) . '?s=' . intval( $size*2 );
 }
