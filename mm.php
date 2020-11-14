ea093a2c68dba9760a8c1fb9f00fca42237ea468
David Sword <david.sword@automattic.com>
2020-11-14 12:43:46 -0800

    add maintance mode

---
+++ mm.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: maintenance mode
+ * Plugin URI: https://github.com/davidsword/davidsword.ca-custom-plugins
+ * Description: disable site
+ * Version: 0.0.1
+ * Author: David Sword
+ * Author URI: https://davidsword.ca/
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+
+add_action('init', 'dsca_mm');
+add_action('rest_api_init', 'dsca_mm');
+
+function dsca_mm() {
+	if ( is_admin() )
+		return;
+
+	die('👋');
+}
