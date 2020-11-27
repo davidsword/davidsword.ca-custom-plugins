e4a0d3f649b72e18ca052c31db6a3dbae7b7bfd0
David Sword <david.sword@automattic.com>
2020-11-27 06:44:53 -0800

    change colour ui

--- change-tune-down-admin-ui.php
+++ change-tune-down-admin-ui.php
@@ -12,6 +12,9 @@
 
  add_action( 'admin_head', function() {
 	 //@todo check current page.
+	 global $_wp_admin_css_colors;
+	 $admin_color = get_user_option( 'admin_color' );
+	 $colors = $_wp_admin_css_colors[$admin_color]->colors;
 	 ?>
 		<style>
 			html body .yoast_premium_upsell,
@@ -21,6 +24,11 @@
 			html body .yoast-container__configuration-wizard,
 			html body #toplevel_page_wpseo_dashboard .update-plugins {
 				display: none !important;
+			}
+
+			html body .switch-light.switch-yoast-seo a,
+			html body .switch-toggle.switch-yoast-seo a {
+				background: <?php echo esc_attr( $colors[2] ) ?>;
 			}
 		</style>
 	 <?php
