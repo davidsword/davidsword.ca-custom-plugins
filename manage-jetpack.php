5817ebd915243499a960e79302ee529ca1d66eb8
David Sword <david+githubla@davidsword.ca>
2020-12-14 06:55:26 -0800

    add isset checks to stop warnings

--- manage-jetpack.php
+++ manage-jetpack.php
@@ -14,7 +14,7 @@
 add_action(
 	'admin_notices',
 	function () {
-		if ( ! $_GET['jp_show_active_modules'] )
+		if ( ! isset( $_GET['jp_show_active_modules'] ) )
 			return;
 		?>
 		<div class="notice notice-info">
@@ -43,7 +43,7 @@ add_filter( 'option_jetpack_active_modules', function ( $modules ) {
 
 // a 1720x image is 3mb from the cdn without this.
 add_filter( 'jetpack_photon_pre_args', function ( $args ) {
-	if ( $args['w'] > 1200 )
+	if ( isset( $args['w'] ) && $args['w'] > 1200 )
 		$args['w'] = 1200;
 	$args['quality'] = 70; // @TODO only reduce when $args reveal its an image >1000px
 	$args['strip']   = 'all';
