113a0ee41dc2f833b6340a2ef54b42435c01e3ef
davidsword <1996370+davidsword@users.noreply.github.com>
2022-01-31 14:57:04 -0800

    reenable REST for wordpress app

--- inc/remove-core-templates-and-rest.php
+++ inc/remove-core-templates-and-rest.php
@@ -28,17 +28,18 @@ add_filter( 'rest_endpoints', function( $endpoints ) {
 	// return array( );
 	return $endpoints;
 } );
+
 add_filter( 'rest_authentication_errors', function( $result ) {
 	// If a previous authentication check was applied, pass that result along without modification.
-	if ( true === $result || is_wp_error( $result ) ) {
-		return $result;
-	}
-	if ( ! is_user_logged_in() ) {
-		return new WP_Error(
-			'rest_not_logged_in',
-			__( 'You are not currently logged in.' ),
-			array( 'status' => 401 )
-		);
-	}
+	// if ( true === $result || is_wp_error( $result ) ) {
+	// 	return $result;
+	// }
+	// if ( ! is_user_logged_in() ) {
+	// 	return new WP_Error(
+	// 		'rest_not_logged_in',
+	// 		__( 'You are not currently logged in.' ),
+	// 		array( 'status' => 401 )
+	// 	);
+	// }
 	return $result;
 });
